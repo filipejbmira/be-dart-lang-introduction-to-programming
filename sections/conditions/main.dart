4de02408ad2c6246d06c6c9b40e00f9174d1d55e
Americo Tomas <americo.tomas@fuseuniversal.com>
2018-06-15 23:00:38 +0100

    Dart: conditions

---
+++ sections/conditions/main.dart
@@ -0,0 +1,9 @@
+main() {
+  const name = 'Tomas';
+  print('Name: ${name}');
+  if (name == 'Americo') {
+    print('Printing the IF: I LOVE technology');
+  } else {
+    print('Printing the Else: FOOTBALL!! #young magicians generation');
+  }
+}
